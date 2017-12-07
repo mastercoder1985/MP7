54a08699a306f0b8c2416251f4c96596250e8044
Thomas <falco@DESKTOP-FLBU63N>
2017-12-07 15:45:06 -0600

    I have a rough draft of how the blackjack algorithm will work.
    Will need to try to correct Hand class.

--- src/cards/Games.java
+++ src/cards/Games.java
@@ -14,6 +14,7 @@ public class Games {
 	 */
 	public String blackjack() {
 		boolean hitOrHold = true;
+		boolean simCom = true;
 		Scanner user = new Scanner(System.in);
 		String response = user.next();
 		System.out.println("Would you like to play blackjack yes or no");
@@ -36,24 +37,47 @@ public class Games {
 					if (otherResponse.toLowerCase() == "hit") { //if user responds with hit
 						userHand.addCard(gameDeck.draw());
 						if (userHand.getHandValue() > 21) {
-							return "sorry you lost. You busted and went over 21."; 
+							return "sorry you lost. You busted and went over 21.";
+
 						} else {
 							System.out.println("Your new hand is: " + userHand.getHandValue());
 						}
 					} else if(otherResponse.toLowerCase() == "hold") {
 						System.out.println("You choose to hold");
 						break;
+					} else {
+						System.out.println("Please choose to hold or hit");
 					}
+				} // end of hit/hold process
+			}
+			while (simCom) { //start of the computer's hand
+				if (comHand.getHandValue() >= 15) {
+					break;
 				}
-				
-				
-			}			
-		} else if (response.toLowerCase() == "no") {
+				if (comHand.getHandValue() < 15) {
+					comHand.addCard(gameDeck.draw());
+				} else if (comHand.getHandValue() > 21) {
+					return "The computer busted and went over 21. You win!";
+				} // hand of the com Hand.
+			}
+			//Start of the comparison. And states winner of the game
+			System.out.println("The winner is...");
+			if (userHand.getHandValue() > comHand.getHandValue()) {
+				return "YOU. Your card value was: " + userHand.getHandValue() + "\n While the computer's hand was: " + comHand.getHandValue();
+			} else if (userHand.getHandValue() < comHand.getHandValue()) {
+				return "The computer. Your card value was: " + userHand.getHandValue() + "\n While the computer's hand was: " + comHand.getHandValue(); 
+			} else {
+				return "NOBODY. You and the computer tied with a value of: " + userHand.getHandValue();
+			}
+		}
+		//other responses
+		if (response.toLowerCase() == "no") {
 			return "I guess you don't want to play then."; 
 		} else {
-			return "Please type yes or no";
+			System.out.println("Please type yes or no");
 		}
-		return "";// will fix later.
+		
+
+		return ""; // will fix later.
 	}
-	
 }
