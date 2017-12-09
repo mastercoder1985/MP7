fc9f79db9baa52bb1ac28a4d5b61d2a7c2421a2c
Andrew <atburke1985@gmail.com>
2017-12-09 15:23:21 -0600

    Comment modification

--- src/cards/Games.java
+++ src/cards/Games.java
@@ -8,15 +8,15 @@ public class Games {
 
 	public Deck gameDeck;
 	public Hand userHand;
-	public Hand comHand; //hand for the computer.
+	public Hand comHand;
 	/** method used to play blackjack.
 	 *
 	 * @returns a statement that says if the user has lost or won.
 	 */
-	public String blackjack(Scanner user) {
+	public String blackjack(Scanner user) throws InterruptedException {
 
 			System.out.println("The game is about to begin");
-			//Game start. Some things that will be called haven't been made or finished yet.
+			//Game start.
 			
 			gameDeck = new Deck();
 			userHand = new Hand("Player");
@@ -47,7 +47,8 @@ public class Games {
 					}
 				} // end of hit/hold process
 				//Dealer's turn
-				
+				System.out.println("Dealer's turn.");
+				Thread.sleep(1000);
 				comHand = new Hand("Dealer");
 				comHand.add(gameDeck.draw());
 				comHand.add(gameDeck.draw());
@@ -57,9 +58,12 @@ public class Games {
 				}
 				
 				while (true) {
+					Thread.sleep(1000);
 					if(comHand.getHandValue() >= 16) {
+						System.out.println("The dealer holds.");
 						break;
 					}
+					System.out.println("The dealer hits.");
 					comHand.add(gameDeck.draw());
 					comHand.printBlackJackHand();
 				}
@@ -67,7 +71,7 @@ public class Games {
 					return"The dealer busted! You win.";
 				}
 			}
-			//Start of the comparison. And states winner of the game
+			//If neither player busted, find winner
 			if (userHand.getHandValue() > comHand.getHandValue()) {
 				return "You won!";
 			} else if (userHand.getHandValue() < comHand.getHandValue()) {
